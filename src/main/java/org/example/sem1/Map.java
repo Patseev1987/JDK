f7f12f9a32e3521f8e13093f2811f9c11ca9a92c
Bogdan Patseev <patseev1987@gmail.com>
2023-11-16 22:37:31 +0300

    need refactoring

--- src/main/java/org/example/sem1/Map.java
+++ src/main/java/org/example/sem1/Map.java
@@ -4,32 +4,125 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Random;
 
 public class Map extends JPanel {
 
+    private static final int DOT_PADDING = 5;
+
+    private int gameOverType;
+    private static final int STATE_DRAW = 0;
+    private static final int STATE_WIN_HUMAN =1;
+    private static final int STATE_WIN_AI =2;
+
+
+    private static final String MSG_WIN_HUMAN = " Human win";
+    private static final String MSG_WIN_AI = " Ai win";
+    private static final String MSG_DRAW = "Draw";
     private int panelWidth;
     private int panelHeight;
     private int cellHeight;
     private int cellWidth;
-    Map(){
+
+
+    private static final Random RANDOM = new Random();
+    private final int HUMAN_DOT = 1;
+    private final int AI_DOT = 2;
+    private final int EMPTY_DOT = 0;
+    private int fieldSizeY = 3;
+    private int fieldSizeX = 3;
+
+    private char[][] field;
+
+    Map() {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
-                 update(e);
+                update(e);
             }
         });
     }
 
-    private void update(MouseEvent e){
-        repaint();
-        int cellX = e.getX()/cellWidth;
-        int cellY = e.getY()/cellHeight;
-        System.out.printf(" x = %d    y = %d",cellX,cellY);
+    private void initMap() {
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                field[i][j] = EMPTY_DOT;
+            }
+        }
     }
 
-    void startNewGame(int mode, int fSx, int fSy, int wLength){
-        System.out.println(mode+fSx+fSy+wLength);
+    private boolean isValidCell(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
+    }
+
+    private boolean isEmptyCell(int x, int y) {
+        return field[x][y] == EMPTY_DOT;
+    }
+
+    private void aiTurn() {
+        int x, y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(x, y));
+        field[y][x] = AI_DOT;
+    }
+
+    private boolean checkWin(char c) {
+        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
+        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
+        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+
+        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
+        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
+        if (field[0][2] == c && field[1][1] == c && field[2][2] == c) return true;
+
+        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
+        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
+        return false;
+    }
+
+    private boolean isMapFull() {
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (field[i][j] == EMPTY_DOT) return false;
+            }
+        }
+        return true;
+    }
+
+
+    private void update(MouseEvent e) {
+        int cellX = e.getX() / cellWidth;
+        int cellY = e.getY() / cellHeight;
+        if (!isValidCell(cellX, cellY) || !isEmptyCell(cellX, cellY)) return;
+        field[cellX][cellY] = HUMAN_DOT;
         repaint();
+
+        if(checkEndGame(AI_DOT,STATE_WIN_AI)) return;
+    }
+
+    private boolean checkEndGame(int dot, int gameOverType){
+        if(checkWin((char) dot)){
+            this.gameOverType = gameOverType;
+            repaint();
+            return true;
+        }
+        if (isMapFull()){
+            this.gameOverType = STATE_DRAW;
+            repaint();
+            return true;
+        }
+        return false;
+    }
+
+    void startNewGame(int mode, int fSx, int fSy, int wLength) {
+        System.out.println(mode + fSx + fSy + wLength);
+        repaint();
+        initMap();
     }
 
     @Override
@@ -38,20 +131,42 @@ public class Map extends JPanel {
         render(g);
     }
 
-    private void render(Graphics g){
+    private void render(Graphics g) {
         panelHeight = getHeight();
         panelWidth = getWidth();
-        cellHeight=panelHeight/3;
-        cellWidth=panelWidth/3;
+        cellHeight = panelHeight / 3;
+        cellWidth = panelWidth / 3;
 
         g.setColor(Color.BLACK);
         for (int w = 0; w < 3; w++) {
             int x = w * cellWidth;
-            g.drawLine(x,0,x,panelHeight);
+            g.drawLine(x, 0, x, panelHeight);
         }
         for (int h = 0; h < 3; h++) {
             int y = h * cellHeight;
-            g.drawLine(0,y,panelWidth,y);
+            g.drawLine(0, y, panelWidth, y);
+        }
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            for (int j = 0; j < fieldSizeX; j++) {
+                if (field[i][j] == EMPTY_DOT) continue;
+
+                if (field[i][j] == HUMAN_DOT) {
+                    g.setColor(Color.BLUE);
+                    g.fillOval(j * cellWidth + DOT_PADDING,
+                            i * cellHeight + DOT_PADDING,
+                            cellWidth - DOT_PADDING * 2,
+                            cellHeight - DOT_PADDING * 2);
+                } else if (field[i][j] == AI_DOT) {
+                    g.setColor(new Color(0xff0000));
+                    g.fillOval(j * cellWidth + DOT_PADDING,
+                            i * cellHeight + DOT_PADDING,
+                            cellWidth - DOT_PADDING * 2,
+                            cellHeight - DOT_PADDING * 2);
+                } else {
+                    throw new RuntimeException("Unexpected value " + field[i][j] + " in cell: x=" + j + " y=" + i);
+                }
+            }
         }
 
     }
