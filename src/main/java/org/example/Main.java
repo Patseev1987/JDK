5588ba7fe842a7e13fadc86ab4855e8141e5f321
Bogdan Patseev <patseev1987@gmail.com>
2023-12-07 14:11:49 +0300

    lesson 3 hw task1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import org.example.sem3.Task2;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Task2<Integer> numbers = new Task2<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+
+        numbers.remove(2);
+
+        System.out.println(numbers.getItemsSize());
+        System.out.println(numbers);
+        System.out.println("****************************");
+
+        Task2.BogdanIterator iterator = numbers.getIterator();
+
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+
+
     }
 }
--- src/main/java/org/example/sem1/tasks/Task1.java
+++
@@ -1,7 +0,0 @@
-package org.example.sem1.tasks;
-
-import javax.swing.*;
-
-public class Task1 extends JFrame {
-
-}
