547f7fab882c13fc260d13ddca5b864bf53fcdca
Bogdan Patseev <patseev1987@gmail.com>
2023-12-07 15:37:23 +0300

    lesson 3 hw task2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,37 @@
 package org.example;
 
+import org.example.sem3.Calculator;
 import org.example.sem3.Task2;
 
 public class Main {
     public static void main(String[] args) {
-        Task2<Integer> numbers = new Task2<>();
+//        Task2<Integer> numbers = new Task2<>();
+//
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(4);
+//        numbers.add(5);
+//        numbers.add(6);
+//
+//        numbers.remove(2);
+//
+//        System.out.println(numbers.getItemsSize());
+//        System.out.println(numbers);
+//        System.out.println("****************************");
+//
+//        Task2.BogdanIterator iterator = numbers.getIterator();
+//
+//
+//        while (iterator.hasNext()){
+//            System.out.println(iterator.next());
+//        }
 
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
-
-        numbers.remove(2);
-
-        System.out.println(numbers.getItemsSize());
-        System.out.println(numbers);
-        System.out.println("****************************");
-
-        Task2.BogdanIterator iterator = numbers.getIterator();
-
-
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
-
-
+        System.out.println(Calculator.sum(10,10f));
+        System.out.println(Calculator.divide(10,10.0));
+        System.out.println(Calculator.multiply(10L,10));
+        System.out.println(Calculator.subtract(10L,10f));
+        System.out.println(Calculator.divide(10L,0));
 
 
     }
---
+++ src/main/java/org/example/sem3/Calculator.java
@@ -0,0 +1,27 @@
+package org.example.sem3;
+
+public  class Calculator {
+    private Calculator() {
+    }
+
+    public static <T extends Number, U extends Number> double sum(T num1, U num2 ){
+        return num1.doubleValue() + num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double subtract(T num1, U num2 ){
+        return num1.doubleValue() - num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double multiply(T num1, U num2 ){
+        return num1.doubleValue() * num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double divide(T num1, U num2 ){
+        if (num2.doubleValue() != 0) {
+            return num1.doubleValue() / num2.doubleValue();
+        }else {
+            throw new ArithmeticException("You can't divide by 0");
+        }
+
+    }
+}
