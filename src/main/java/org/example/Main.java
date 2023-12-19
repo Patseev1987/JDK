8f1434fd38ed92321ee5f7cedf616a8d983726bc
Bogdan Patseev <patseev1987@gmail.com>
2023-12-19 22:48:32 +0300

    add sem6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,9 @@
 package org.example;
 
 
-import org.example.sem4.Employee;
-import org.example.sem4.EmployeesDictionary;
-
-import java.time.LocalDate;
-import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
-        EmployeesDictionary employees = EmployeesDictionary.getInstance();
-        employees.addEmployee(new Employee(1,"3333","John",LocalDate.of(2012,12,12)));
-        employees.addEmployee(new Employee(2,"3667","Franc",LocalDate.of(2020,2,1)));
-        employees.addEmployee(new Employee(3,"8899","Coule",LocalDate.of(2018,3,17)));
-        employees.addEmployee(new Employee(4,"000000","Bob",LocalDate.of(2021,5,26)));
-        employees.addEmployee(new Employee(5,"776859403","Thomas",LocalDate.of(2009,4,7)));
-        employees.showAllEmployees();
-
-        EmployeesDictionary emp2 = EmployeesDictionary.getInstance();
-        emp2.showAllEmployees();
-
-        employees.searchPhoneNumberByName("John");
-        emp2.showResultsByExperienceSearch();
-
-        employees.showEmployeeByPersonalNumber(3);
-
-
-        emp2.showEmployeeByPersonalNumber(100);
 
 
     }
